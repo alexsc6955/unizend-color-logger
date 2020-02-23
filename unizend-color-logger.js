f970c966f876a712c3199a5bc1475ffdf609f77c
Santiago Rincon <rincorpes@gmail.com>
2020-02-23 11:51:24 -0500

    Added: Background Colors Changed: Now you can change the color using  instead of Deprecated: , ,

--- unizend-color-logger.js
+++ unizend-color-logger.js
@@ -1,5 +1,122 @@
+// Copyright (c) 2019 Santiago Rincón
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+"use strict"
+
+const originalLogger = console.log,
+    fs = require('fs'), // Only for debugging purposes
+    writeStream = fs.createWriteStream('logs.txt') // Only for debugging purposes
+
+/**
+ * unizend-color-logger Object object
+ * 
+ * @since  0.2.0
+ */
 const uzl = {}
 
+/**
+ * Text colors
+ * 
+ * @since  0.2.0
+ */
+uzl.fgColors = {
+  black: [30, 39],
+  red: [31, 39],
+  green: [32, 39],
+  yellow: [33, 39],
+  blue: [34, 39],
+  magenta: [35, 39],
+  cyan: [36, 39],
+  white: [37, 39],
+  gray: [90, 39]
+}
+
+/**
+ * Bright colors
+ * 
+ * TODO make it work
+ * 
+ * @since  0.2.0
+ */
+uzl.bright = {
+  colors: {
+    red: [91, 39],
+    green: [92, 39],
+    yellow: [93, 39],
+    blue: [94, 39],
+    magenta: [95, 39],
+    cyan: [96, 39],
+    white: [97, 39]
+  },
+  bgColors: {
+    black: [100, 49],
+    red: [101, 49],
+    green: [102, 49],
+    yellow: [103, 49],
+    blue: [104, 49],
+    magenta: [105, 49],
+    cyan: [106, 49],
+    white: [107, 49]
+  }
+}
+
+/**
+ * Background colors
+ * 
+ * @since  0.2.0
+ */
+uzl.bgColors = {
+  black: [40, 49],
+  red: [41, 49],
+  green: [42, 49],
+  yellow: [43, 49],
+  blue: [44, 49],
+  magenta: [45, 49],
+  cyan: [46, 49],
+  white: [47, 49]
+}
+
+/**
+ * Styles
+ * 
+ * @since  0.2.0
+ */
+uzl.styles = {
+  reset: [0, 0],
+  bold: [1, 22],
+  dim: [2, 22],
+  italic: [3, 23],
+  underline: [4, 24],
+  inverse: [7, 27],
+  hidden: [8, 28],
+  strikethrough: [9, 29]
+}
+
+/**
+ * Style actions
+ * 
+ * // Deprecated
+ * 
+ * @since 0.1.0
+ * @deprecated 0.2.0
+ */
 uzl.actions = {
   reset: "\x1b[0m",
   bright: "\x1b[1m",
@@ -10,6 +127,14 @@ uzl.actions = {
   hidden: "\x1b[8m"
 }
 
+/**
+ * Text colors
+ * 
+ * // Deprecated
+ * 
+ * @since 0.1.0
+ * @deprecated 0.2.0
+ */
 uzl.colors = {
   black: "\x1b[30m",
   red: "\x1b[31m",
@@ -21,6 +146,14 @@ uzl.colors = {
   white: "\x1b[37m"
 }
 
+/**
+ * Style actions
+ * 
+ * // Deprecated
+ * 
+ * @since 0.1.0
+ * @deprecated 0.2.0
+ */
 uzl.log = (action, message) => {
   let style
   let msg
@@ -63,19 +196,38 @@ uzl.log = (action, message) => {
       msg = action
   }
   
-  return (style) ? console.log(style, msg) & console.log(uzl.actions.reset) : console.log(msg)
+  return (style) ? console.log(style, msg + ' uzl.log is deprecated and will be removed in future versions') & console.log(uzl.actions.reset) : console.log(msg + ' uzl.log is deprecated and will be removed in future versions')
 }
 
-// uzl.log('Hello, World!')
+/**
+ * Change console.log behavior
+ * 
+ * @since 0.2.0
+ */
+console.log = function () {
 
-// uzl.log('blue', 'Turning Blue!')
+  if (arguments.length > 1) {
+    let options = arguments[0].split('.')
 
-// uzl.log('blue')
+    for (let i = 0; i < options.length; i++) {
+      if (uzl.colors[options[i]]) {
+        arguments[0] = '\x1b[' + uzl.colors[options[i]][0] + 'm' + options[i] + '\x1b[' + uzl.colors[options[i]][1] + 'm'
+        writeStream.write(arguments[0])
+        break
+      } else if (options[i].substr(0, 3) === 'bg-' && uzl.bgColors[options[i].substr(options[i].indexOf('-') + 1)]) {
+        arguments[0] = '\x1b[' + uzl.bgColors[options[i].substr(options[i].indexOf('-') + 1)][0] + 'm' + options[i].substr(options[i].indexOf('-') + 1) + '\x1b[' + uzl.bgColors[options[i].substr(options[i].indexOf('-') + 1)][1] + 'm'
+        writeStream.write(arguments[0])
+        break
+      } else if (uzl.styles[options[i]]) {
+        arguments[0] = '\x1b[' + uzl.styles[options[i]][0] + 'm' + options[i] + '\x1b[' + uzl.styles[options[i]][1] + 'm'
+        writeStream.write(arguments[0])
+        break
+      }
+    }
+  }
 
-// uzl.log('blue sea')
-
-// uzl.log('Testing reset')
-
-// uzl.log('cyan', 'Displaying the color you see!')
+  // You can still call the original `console.log`, with all the `arguments`
+  originalLogger.apply(this, arguments);
+}
 
 module.exports = uzl
